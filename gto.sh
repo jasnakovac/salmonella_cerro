2543425126160b45360e1da08eb514b4b5d892f8
jasnakovac <jk2739@cornell.edu>
2017-02-03 13:59:26 -0500

    Add files via upload

---
+++ gto.sh
@@ -0,0 +1,21 @@
+#RASTtk batch genome annotation
+
+#Open RASTtk app
+#Login from the command line
+kbase-login <username>
+
+#Move all the contig files in the ToSubmit directory
+#Convert fasta files into genome typed objects
+cd <path to ToSubmit directory>
+
+for f in *.fasta 
+do
+rast-create-genome --scientific-name "${f%.fasta}" --genetic-code 11 --domain Bacteria --contigs $f > ToSubmit/${f%.fasta}
+done
+
+#Submit a genome batch for annotation (NOTE: there is a limit for the number of characters in file names)
+rast-process-genome-batch ToSubmit 
+
+#Save a job id
+#Use a job id to follow a job progress
+rast-query-genome-batch <job_id> > rast_job_id.txt
