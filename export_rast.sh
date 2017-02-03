63ae1fc6156ee3a5050995f1d7ebc3c0e4f26677
jasnakovac <jk2739@cornell.edu>
2017-02-03 14:06:17 -0500

    Add files via upload

---
+++ export_rast.sh
@@ -0,0 +1,22 @@
+#RASTtk annotation spreadsheet and gbk files export
+
+#Open RASTtk app
+#Login from the command line
+kbase-login <username>
+
+#Export RAST-style spreadsheets; tab-separated text format
+mkdir spreadsheets # in the Output directory
+for f in *.gto
+do
+rast-export-genome spreadsheet_txt < ${f%.gto}.gto > spreadsheets/${f%.gto}_spreadsheet.txt
+done
+
+#Export genbank files
+mkdir genbank # in the Output directory
+for f in *.gto
+do
+rast-export-genome genbank_merged < ${f%.gto}.gto > genbank/${f%.gto}.gbk
+done
+
+#Extract unique genes for building the pangenome presence/absence matrix
+cat *.txt|awk -F"\t" '{print " "$8" "}'|sort|uniq > unique.txt
